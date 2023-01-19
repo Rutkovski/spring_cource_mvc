647ede642da961a8254d5e11c781a8cb1d51505a
Rutkovski Dmitriy <rutkovski.dp@gmail.com>
2023-01-19 20:19:18 +0300

    Initial commit

---
+++ src/main/java/com/rutkovski/spring/mvc/MyController.java
@@ -0,0 +1,15 @@
+package com.rutkovski.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/")
+    public String showFirsView(){
+        return "first-view";
+    }
+
+}
